ce9fb43d22631cf756394d3806b58a969f8b6385
Данила Птуха <dptuha1406@gmail.com>
2021-10-16 19:18:57 +0300

    first commit

---
+++ cmd/server/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"flag"
+	"go_serv/internal/app/server"
+	"log"
+
+	"github.com/BurntSushi/toml"
+)
+
+var (
+	configPath string
+)
+
+func init() {
+	flag.StringVar(&configPath, "config", "configs/server.toml", "Path to server config")
+}
+
+func main() {
+	flag.Parse()
+
+	config := server.NewConfig()
+	_, err := toml.DecodeFile(configPath, config)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	s := server.New(config)
+	if err := s.Start(); err != nil {
+		log.Fatal(err)
+	}
+}
---
+++ internal/app/server/server.go
@@ -0,0 +1,73 @@
+package server
+
+import (
+	"go_serv/internal/app/store"
+	"io"
+	"net/http"
+
+	"github.com/gorilla/mux"
+	"github.com/sirupsen/logrus"
+)
+
+type Server struct {
+	config *Config
+	logger *logrus.Logger
+	router *mux.Router
+	store  *store.Store
+}
+
+func New(config *Config) *Server {
+	return &Server{
+		config: config,
+		logger: logrus.New(),
+		router: mux.NewRouter(),
+	}
+}
+
+func (s *Server) Start() error {
+	if err := s.configureLogger(); err != nil {
+		return err
+	}
+
+	s.configureRouter()
+
+	if err := s.configureStore(); err != nil {
+		return err
+	}
+
+	s.logger.Info("Strarting server")
+
+	return http.ListenAndServe(s.config.BindAddr, s.router)
+}
+
+func (s *Server) configureLogger() error {
+	level, err := logrus.ParseLevel(s.config.LogLevel)
+	if err != nil {
+		return err
+	}
+
+	s.logger.SetLevel(level)
+
+	return nil
+}
+
+func (s *Server) configureRouter() {
+	s.router.HandleFunc("/hello", s.handleHello())
+}
+
+func (s *Server) configureStore() error {
+	st := store.New(s.config.Store)
+	if err := st.Open(); err != nil {
+		return err
+	}
+
+	s.store = st
+	
+	return nil
+}
+
+func (s *Server) handleHello() http.HandlerFunc {
+	return func(rw http.ResponseWriter, r *http.Request) {
+		io.WriteString(rw, "hello")
+	}
+}
